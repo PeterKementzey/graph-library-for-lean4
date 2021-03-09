90db1d40fe31be3bee81bef45e46af7ae04c1502
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-03-09 18:39:43 +0100

    Try to implement 'addVertex', need to fix 'environment does not contain Graph.lt'

--- Graph/graph-representation.lean
+++ Graph/graph-representation.lean
@@ -14,16 +14,18 @@ structure Vertex (α : Type) where
 
 
 structure Graph (α : Type) (lt : α → α → Bool) where
-  adjacencyLists : Array (Vertex α) := Array.empty
+  vertices : Array (Vertex α) := Array.empty
 
 
 namespace Graph
 
 variables {α : Type} {β : α → α → Bool}
 
-
 def addVertex (g : Graph α β) (x : α): Graph α β :=
-  { g with adjacencyLists := g.adjacencyLists.push {node := x} }
+  { g with vertices := match g.vertices.findIdx? (g.lt x) with
+    | some index => g.vertices.insertAt index (Vertex x)
+    | none => g.vertices.insertAt 0 (Vertex x)
+   }
 
 -- def mkFromVertexList (vertices: Array α) : γ := 0
 
