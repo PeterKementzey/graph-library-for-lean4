f41ff2966bce4f746beca359dc8800abe326aca1
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-03-11 20:47:48 +0100

    Change to Nat id based representation

--- Graph/graph-representation.lean
+++ Graph/graph-representation.lean
@@ -1,69 +1,38 @@
-/-
-I'm overall a bit confused about the representation. Is the α a unique
-identifier for each vertex?
-
-If so, I would make this a Nat instead of some arbitrary type. Then you can
-store the adjacency information very efficiently as an array A where A[n] is
-the adjacency list of node n. However, you probably want to allow vertices to
-store some information in addition to their unique identifier.
-
-If the α is not supposed to be a unique identifier, then your current definition
-of Edge doesn't make sense to me since the source and target are not uniquely
-identified.
-
-Another way to store the adjacency information would be as a map (e.g. a hash
-map) which maps nodes to the their lists of neighbours. Then your nodes can be
-arbitrary (hashable) things. However, I would expect this to be somewhat slower
-in practice than the array solution, even if the complexity is the same.
-
-Wikipedia [1] has some more details on these representations. Which one you
-choose imo doesn't matter that much for this project so long as it is at least
-reasonably efficient.
-
-[1] https://en.wikipedia.org/wiki/Adjacency_list
--/
-
-structure Edge (α : Type) where
-  source : α
-  target : α
-  -- maybe just add weight here with default value 1?
-
-structure WeightedEdge (α : Type) extends Edge α where
-  weight : Nat
-
+structure Edge where
+  target : Nat
+  weight : Int := 1
 
 structure Vertex (α : Type) where
-  node : α
-  adjacencyList : Array (Edge α) := #[]
-  -- Why is the adjacency list a list of edges? Shouldn't it be a list of
-  -- vertices, since the source vertex of each edge is the current vertex?
+  userData : α
+  adjacencyList : Array Edge := #[]
 
-
-structure Graph (α : Type) (lt : α → α → Bool) where
+structure Graph (α : Type) where
   vertices : Array (Vertex α) := #[]
-
--- Test: can we define a circle with two vertices?
--- Answer: yes, if the current representation is what I think it is.
-partial def circle : Graph Nat (. < .) :=
-  let v0 : Vertex Nat := { node := 0, adjacencyList := #[{source := 0, target := 1}]}
-  let v1 : Vertex Nat := { node := 1, adjacencyList := #[{source := 1, target := 0}]}
-  ⟨#[v0, v1]⟩
-  -- N.B. This is yet another way to construct values of structures. ⟨x, y, z⟩
-  -- is the structure constructor (here Graph.mk) applied to x, y and z.
-
 
 namespace Graph
 
-variables {α : Type} {β : α → α → Bool}
+variables {α : Type}
 
-def addVertex (g : Graph α β) (x : α): Graph α β :=
-  { g with vertices := match g.vertices.findIdx? (g.lt x) with
-    | some index => g.vertices.insertAt index (Vertex x)
-    | none => g.vertices.insertAt 0 (Vertex x)
-   }
-
--- def mkFromVertexList (vertices: Array α) : γ := 0
+def addVertex (g : Graph α) (x : α): Graph α := {
+  g with vertices := g.vertices.push {userData := x}
+}
 
 -- def addEdge
 
 end Graph
+
+
+
+
+
+
+
+-- Test: can we define a circle with two vertices?
+-- Answer: yes, if the current representation is what I think it is.
+partial def circle : Graph Char :=
+  let v0 : Vertex Char := { userData := 'a', adjacencyList := #[{target := 1}]}
+  let v1 : Vertex Char := { userData := 'b', adjacencyList := #[{target := 0}]}
+  ⟨#[v0, v1]⟩
+  -- N.B. This is yet another way to construct values of structures. ⟨x, y, z⟩
+  -- is the structure constructor (here Graph.mk) applied to x, y and z.
+  -- Peter: this is cool but this character is not present on my US international keyboard :DD is there a backslash shortcut for it?
