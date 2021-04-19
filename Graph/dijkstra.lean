67820d43b6940f35538d68498381ec05a0af8819
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-04-19 15:39:06 +0200

    Move dijstra full tree and dijstra short circuited to separate functions

--- Graph/dijkstra.lean
+++ Graph/dijkstra.lean
@@ -10,6 +10,7 @@ inductive Path (α : Type _) : Bool → Type _ where
  | edge : Nat -> Path α true -> Path α false
  | empty : Path α false
 
+
 private def findMinimum (set : Std.HashSet Nat) (distances : Array ((Option Nat) × Nat)) : Nat := 
   let min : Option Nat -> Nat -> Option Nat := fun leftIdOption rightId => match leftIdOption with 
     | none => some rightId
@@ -25,6 +26,7 @@ private def findMinimum (set : Std.HashSet Nat) (distances : Array ((Option Nat)
   match set.fold min none with
     | none => panic! "this should not be possible"
     | some temp => temp
+
 
 -- Note for thesis :Fuel pattern - give enough fuel to always treminate
 private def dijkstraAux (g : Graph α) (current : Nat) (target : Option Nat) (unvisited : Std.HashSet Nat) (distanceAndPredecessor : Array ((Option Nat) × Nat)) : Nat -> Array ((Option Nat) × Nat)
@@ -48,8 +50,8 @@ private def dijkstraAux (g : Graph α) (current : Nat) (target : Option Nat) (un
         | none => distances
         | some x => dijkstraAux g nextCurrent target (unvisited.erase nextCurrent) distances n
 
--- TODO create wrapper for array opt nat nat with funtcions to return specific paths from the tree or the whole tree
-def dijkstraUnsafe (g : Graph α) (source : Nat) (target : Option Nat := none) : Array ((Option Nat) × Nat) :=
+
+private def dijkstraAuxBase (g : Graph α) (source : Nat) (target : Option Nat) : Array ((Option Nat) × Nat) :=
   let distanceAndPredecessorInitial : Array ((Option Nat) × Nat) := mkArray g.vertices.size (none, source) -- (weight, parent) pairs initialized to (infinitiy, placeholder)
   if h : source < distanceAndPredecessorInitial.size then
     let distanceAndPredecessor := distanceAndPredecessorInitial.set ⟨source, h⟩ (some 0, source)
@@ -67,7 +69,11 @@ def dijkstraUnsafe (g : Graph α) (source : Nat) (target : Option Nat := none) :
       panic! "source out of bounds"
 
 
--- def dijkstraSafe TODO
+-- TODO create wrapper for array opt nat nat with funtcions to return specific paths from the tree or the whole tree
+def dijkstraUnsafe (g : Graph α) (source : Nat) : Array ((Option Nat) × Nat) := dijkstraAuxBase g source none
 
+def dijkstraUnsafeWithDestination (g : Graph α) (source : Nat) (target : Nat) : Array ((Option Nat) × Nat) := dijkstraAuxBase g source (some target)
+
+-- def dijkstraSafe TODO
 
 end Graph
