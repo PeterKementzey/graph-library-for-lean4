df7d9db8b5e87bb9a258ded6cf955660b5b2a129
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-04-22 21:09:20 +0200

    Initial attempt at shared structure

--- Graph/basicsearch.lean
+++ Graph/basicsearch.lean
@@ -1,9 +1,48 @@
 import Graph.graphrepresentation
 import Std.Data.Queue
+import Std.Data.Stack
 
 namespace Graph
 
 variable {α : Type} [BEq α] [Inhabited α]
+
+#check Std.Queue
+
+universe u
+
+private structure Container (β : Type u) (containerType : Type u -> Type u) where
+  χ := containerType β
+  container : χ
+  addFun :χ -> β -> χ
+  removeFun : χ -> χ
+  get : χ -> β
+  -- getFun : χ -> β
+
+namespace Container
+
+def add (cont : Container b c) (x : b) : Container b c := {
+  cont with container := cont.addFun cont.container x
+}
+
+def remove (cont : Container b c) : Container b c := {
+  cont with container := cont.removeFun cont.container
+}
+
+-- def get (cont : Container b c) : b :=
+--   cont.getFun cont.container
+
+
+#check Std.Stack
+
+private def testing : Nat -> List Nat := do
+  let mut container : Container Nat Std.Stack := { container := Std.Stack.empty, addFun := Std.Stack.push, removeFun := Std.Stack.pop, get := Std.Stack.peek! }
+  container := container.add 3
+
+  _
+
+
+
+end Container
 
 -- TODO make it generic stack - queue (tip: bundle them into a structure which is either stack or queue)
 private def BFSAux (g : Graph α) (target : Nat) (visited : Array Bool) (q : Std.Queue Nat) : Nat -> Bool
