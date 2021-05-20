99a4ee168077010efecb6785a8fa5ae73f6f0af8
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-05-20 15:34:07 +0200

    Comment out TopologicalSort.lean for the time being

--- Graph/TopologicalSort.lean
+++ Graph/TopologicalSort.lean
@@ -9,33 +9,33 @@ structure State where
   permanentMark : Array Bool
   res : Array Nat
 
-private def visit (g : Graph α) (id : Nat) (state : State) : Option State :=
-  -- Arrive at node
-  if state.permanentMark[id] then return some state else
-  if state.temporaryMark[id] then return none else
+-- private def visit (g : Graph α) (id : Nat) (state : State) : Option State :=
+--   -- Arrive at node
+--   if state.permanentMark[id] then return some state else
+--   if state.temporaryMark[id] then return none else
 
-  let stateWithCurrentTempMarked := { state with
-    temporaryMark := state.temporaryMark.set! id true
-  }
+--   let stateWithCurrentTempMarked := { state with
+--     temporaryMark := state.temporaryMark.set! id true
+--   }
 
-  do
-    let mut s := stateWithCurrentTempMarked
-    for edge in g.vertices[id].adjacencyList do -- Depth first iteration
-      match visit g edge.target s with
-        | some x => s := x
-        | none => return none
+--   do
+--     let mut s := stateWithCurrentTempMarked
+--     for edge in g.vertices[id].adjacencyList do -- Depth first iteration
+--       match visit g edge.target s with
+--         | some x => s := x
+--         | none => return none
     
-    -- Leave node
-    let stateWithCurrentPermMarked := { s with
-      temporaryMark := s.temporaryMark.set! id false
-      permanentMark := s.permanentMark.set! id true
-      res := s.res.push id
-    }
+--     -- Leave node
+--     let stateWithCurrentPermMarked := { s with
+--       temporaryMark := s.temporaryMark.set! id false
+--       permanentMark := s.permanentMark.set! id true
+--       res := s.res.push id
+--     }
 
-    return some stateWithCurrentPermMarked
+--     return some stateWithCurrentPermMarked
   
-def topologicalSort (g : Graph α) : Array Nat :=
-  -- visit a node without a temporary mark while there are any
-  _
+-- def topologicalSort (g : Graph α) : Array Nat :=
+--   -- visit a node without a temporary mark while there are any
+--   _
 
 end Graph
