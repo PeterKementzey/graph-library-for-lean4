3cb4c458fbfd335ce170185806ec1487da4f9d59
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-05-21 15:58:26 +0200

    Remove completeTraversal option

--- Graph/Traverse.lean
+++ Graph/Traverse.lean
@@ -10,10 +10,10 @@ open Internal
 
 variable {α : Type} [BEq α] [Inhabited α]
 
-private def traverseAux {β : Type _} {containerType : Type _} (g : Graph α) (visited : Array Bool) (container : Container Nat containerType) (state : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) (completeTraversal : Bool) : Nat -> β
+private def traverseAux {β : Type _} {containerType : Type _} (g : Graph α) (visited : Array Bool) (container : Container Nat containerType) (state : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) : Nat -> β
   | 0 => state
   | n + 1 => match container.remove? with
-    | none => state -- Add option to continue from different source node
+    | none => state
     | some (currentNodeId, containerWithNodeRemoved) => do
       let (newState, terminate?) := visit currentNodeId state
       match terminate? with
@@ -24,19 +24,19 @@ private def traverseAux {β : Type _} {containerType : Type _} (g : Graph α) (v
           for neighbor in unvisitedNeighborIds do
             visitedMut := visitedMut.set! neighbor true
           let containerWithNewNodes := containerWithNodeRemoved.addAll unvisitedNeighborIds
-          let stateAfterFurtherSearch := traverseAux g visitedMut containerWithNewNodes newState visit leave completeTraversal n
+          let stateAfterFurtherSearch := traverseAux g visitedMut containerWithNewNodes newState visit leave n
           match leave with
             | some leaveFunction => leaveFunction currentNodeId stateAfterFurtherSearch
             | none => stateAfterFurtherSearch
 
 
-def breadthFirstTraversal (g : Graph α) (source : Nat) (startingState : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) (completeTraversal : Bool := false) : β :=
+def breadthFirstTraversal (g : Graph α) (source : Nat) (startingState : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) : β :=
   let visited : Array Bool := mkArray g.vertices.size false
-  traverseAux g (visited.set! source true) (Container.emptyQueue.add source) startingState visit leave completeTraversal g.vertices.size
+  traverseAux g (visited.set! source true) (Container.emptyQueue.add source) startingState visit leave g.vertices.size
 
-def depthFirstTraversal (g : Graph α) (source : Nat) (startingState : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) (completeTraversal : Bool := false) : β :=
+def depthFirstTraversal (g : Graph α) (source : Nat) (startingState : β) (visit : Nat -> β -> β × Bool) (leave : Option (Nat -> β -> β)) : β :=
   let visited : Array Bool := mkArray g.vertices.size false
-  traverseAux g (visited.set! source true) (Container.emptyStack.add source) startingState visit leave completeTraversal g.vertices.size
+  traverseAux g (visited.set! source true) (Container.emptyStack.add source) startingState visit leave g.vertices.size
 
 
 -- Example use:
