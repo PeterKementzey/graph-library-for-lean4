1368fe6b86759658ce8b3fdfabb84008d7ebd3a2
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-05-21 20:18:07 +0200

    Implement topsort using traversal, need to fix bug

--- Graph/TopologicalSort.lean
+++ Graph/TopologicalSort.lean
@@ -1,4 +1,7 @@
 import Graph.Graph
+import Graph.Traverse
+
+import Graph.StdExtensions
 
 namespace Graph
 
@@ -9,7 +12,40 @@ structure State where
   permanentMark : Array Bool
   res : Array Nat
 
--- private def visit (g : Graph α) (id : Nat) (state : State) : Option State :=
+instance : Inhabited State := ⟨ arbitrary, arbitrary, arbitrary ⟩
+
+private def visit (id : Nat) (s : Option State) : Option State × Bool :=
+  let state := s.get!
+  if state.permanentMark[id] then return (some state, false) else
+  if state.temporaryMark[id] then return (none, true) else
+  
+  let updatedState := { state with
+    temporaryMark := state.temporaryMark.set! id true
+  }
+
+  (some updatedState, false)
+
+private def leave (id : Nat) (s : Option State) : Option State :=
+  let state := s.get!
+  some { s.get! with
+    temporaryMark := state.temporaryMark.set! id false
+    permanentMark := state.permanentMark.set! id true
+    res := state.res.push id
+  }
+
+
+def topologicalSort (g : Graph α) : Option (Array Nat) := 
+  let res : Option State := g.depthFirstTraversal 0 initialState visit leave -- Why does this work? This should be (some leave)
+  match res with
+    | some state => some state.res.reverse
+    | none => none
+  where
+    initialState := some ⟨ (mkArray g.vertices.size false), (mkArray g.vertices.size false), Array.empty ⟩
+
+
+
+
+-- private def visit? (g : Graph α) (id : Nat) (state : State) : Option State :=
 --   -- Arrive at node
 --   if state.permanentMark[id] then return some state else
 --   if state.temporaryMark[id] then return none else
@@ -20,8 +56,8 @@ structure State where
 
 --   do
 --     let mut s := stateWithCurrentTempMarked
---     for edge in g.vertices[id].adjacencyList do -- Depth first iteration
---       match visit g edge.target s with
+--     for edge in g.vertices[id].adjacencyList do -- Depth first traversal
+--       match visit? g edge.target s with
 --         | some x => s := x
 --         | none => return none
     
