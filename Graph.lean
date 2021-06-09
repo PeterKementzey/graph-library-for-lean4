fa2a5199c0902170cb30dd4b5634e2ca9de23963
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-10 01:29:26 +0200

    Updates from meeting and other minor additions

--- Graph.lean
+++ Graph.lean
@@ -21,7 +21,7 @@ this is function documentation
 `code`
 # heading
 -/
-def hi := 5
+def randomNecessaryFunctionForComment := 5
 
 
 
@@ -29,22 +29,44 @@ def hi := 5
 -- def printOne : String := toString (exampleGraph4.depthFirstPathSearch 0 10)
 -- def printTwo : String := toString (exampleGraph4.depthFirstPathSearch 2 10)
 
-def euMails := "../stanford-graphs/email-EuAll.txt"
+-- def euMails := "../stanford-graphs/email-EuAll.txt"
 
-def gnutella8 := "../stanford-graphs/p2p-Gnutella08.txt"
+-- def gnutella8 := "../stanford-graphs/p2p-Gnutella08.txt"
 
-def topSortGraph := "../stanford-graphs/topsort-gen.txt"
-def bugSearchGraph := "../stanford-graphs/bug-search.txt"
-def topSortBugGraph := "../stanford-graphs/bug.txt"
+-- def topSortGraph := "../stanford-graphs/topsort-gen.txt"
+-- def bugSearchGraph := "../stanford-graphs/bug-search.txt"
+-- def topSortBugGraph := "../stanford-graphs/bug.txt"
 
-def mediumSparse := "../stanford-graphs/medium-sized-sparse-topsort-gen.txt"
-def maximumWorkingSize := "../stanford-graphs/maximum-working-size-topsort-gen.txt"
+-- def mediumSparse := "../stanford-graphs/medium-sized-sparse-topsort-gen.txt"
+-- def maximumWorkingSize := "../stanford-graphs/maximum-working-size-topsort-gen.txt"
 
-def main : IO Unit := do
+-- def main : IO Unit := do
 
-  let filePath := maximumWorkingSize
+--   let filePath := maximumWorkingSize
+
+--   let graph <- parseGraphFromEdgeList filePath
+--   -- IO.println (graph)
+--   IO.println (graph.topSort)
+--   -- IO.println (graph.depthFirstTraversalOrderWithLeaving 0)
+
+
+def bitcoinSize := "../generated-graphs/stanford-bitcoin-sized-topsort-gen.txt"
+def maximumSize := "../benchmarking/generated-graphs/maximum-working-size-topsort-gen.txt"
+def mediumDense := "../generated-graphs/medium-dense-topsort-gen.txt"
+def mediumVeryDense := "../generated-graphs/medium-very-dense-topsort-gen.txt"
+def mediumSparse := "../generated-graphs/medium-sparse-topsort-gen.txt"
+def mediumVerySparse := "../generated-graphs/medium-very-sparse-topsort-gen.txt"
+def smallDense := "../generated-graphs/small-dense-topsort-gen.txt"
+def smallSparse := "../generated-graphs/small-sparse-topsort-gen.txt"
+
+def main (argv : List String) : IO Unit := do
+
+  IO.println argv
+  let filePath := maximumSize
 
   let graph <- parseGraphFromEdgeList filePath
   -- IO.println (graph)
-  IO.println (graph.topSort)
   -- IO.println (graph.depthFirstTraversalOrderWithLeaving 0)
+  timeit "message: " do
+    IO.sleep 100
+    -- IO.println (sorted.get![0])
--- Graph/Parser.lean
+++ Graph/Parser.lean
@@ -16,15 +16,15 @@ def toNatPairs (arr : Array String) : Array (Nat × Nat) :=
 def parser (nodeCount : Nat) (input : Array String) : Graph Bool Nat := do
   let edges : Array (Nat × Nat) := toNatPairs input
   let mut gx : Graph Bool Nat := Graph.makeGraphFromArray (mkArray nodeCount false)
-  let mut i := 1
+  -- let mut i := 1
   for edge in edges do
-    gx := gx.addEdgeById edge.1 edge.2 i
-    i := i+1
+    gx := gx.addEdgeById edge.1 edge.2 0 -- i
+    -- i := i+1
   gx
 
 def parseGraphFromEdgeList (filePath : System.FilePath) : IO (Graph Bool Nat) := do
   let input <- IO.FS.lines filePath
   let nodeCount : Nat := match input[0].toNat? with
     | some n => n
-    | none => panic! "Node count not a natural number!"
+    | none => panic! "Node count not a natural number: " ++ input[0]
   parser nodeCount (input.eraseIdx 0)
