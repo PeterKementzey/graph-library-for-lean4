e382a21778cc4dd37dc0cae64647720593ba6272
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-11 23:35:35 +0200

    Implement DFS haskell style - using Tree, super fast but uses partial def

--- Graph/DepthFirstSearch.lean
+++ Graph/DepthFirstSearch.lean
@@ -11,21 +11,45 @@ private def indexArray (size : Nat) : Array Nat := do
 variable {α : Type} [Inhabited α] {β : Type}
 
 inductive Tree (g : Graph α β) where
-  | expandedTree : Nat -> Array (Tree g) -> Tree g
-  | nonExpandedTree : Nat -> Tree g
+  | expandedNode : Nat -> Array (Tree g) -> Tree g
+  | nonExpandedNode : Nat -> Tree g
 
 namespace Tree
 
-def expandTree (t : Tree (g : Graph α β)) (visited : Array Bool) : Tree g := match t with
-  | expandedTree _ _ => t
-  | nonExpandedTree node => 
-      let adjacencyList := (g.vertices[node].adjacencyList.map (λ edge => edge.target)).filter (!visited[.])
-      let subForest := adjacencyList.map (λ n => nonExpandedTree n)
-      expandedTree node subForest
+instance : Inhabited (Tree (g : Graph α β)) where default := nonExpandedNode arbitrary
+
+def expand (t : Tree (g : Graph α β)) (visited : Array Bool) : Tree g := match t with
+  | expandedNode _ _ => t
+  | nonExpandedNode id => 
+      let adjacencyList := (g.vertices[id].adjacencyList.map (λ edge => edge.target)).filter (!visited[.])
+      let subForest := adjacencyList.map (λ n => nonExpandedNode n)
+      expandedNode id subForest
+
+private partial def fullyExpandAux (t : Tree (g : Graph α β)) (visited : Array Bool) : (Tree g) × (Array Bool) := match t with
+  | expandedNode _ _ => (t, visited)
+  | nonExpandedNode id => 
+      let adjacencyList := (g.vertices[id].adjacencyList.map (λ edge => edge.target)).filter (!visited[.])
+      let subForest : Array (Tree g) := adjacencyList.map (λ n => nonExpandedNode n)
+      let (newForest, newVisited) := subForest.foldr aux (#[], visited.set! id true)
+      (expandedNode id newForest, newVisited)
+
+      where aux (t : Tree (g : Graph α β)) (pair : (Array (Tree g)) × (Array Bool)) :=
+        let forest := pair.1
+        let visited := pair.2
+        let (newTree, newVisited) := t.fullyExpandAux visited
+        (forest.push newTree, newVisited)
+
+def fullyExpand (t : Tree (g : Graph α β)) : Tree g := (t.fullyExpandAux (mkArray g.vertices.size false)).1
+
+partial def depthFirstTraversalOrderAux (t : Tree (g : Graph α β)) (arr : Array Int) : Array Int := match t with
+  | nonExpandedNode _ => panic! "This should not be possible!"
+  | expandedNode id subForest => (subForest.foldr depthFirstTraversalOrderAux (arr.push id)).push (id * -1)
 
 end Tree
 
--- def generateTree (g : Graph α β) (node : Nat) : Tree := 
-
+def depthFirstTraversalOrderWithLeaving3 (g : Graph α β) (source : Nat) : Array Int :=
+  let tree : Tree g := Tree.nonExpandedNode source
+  let expanded := tree.fullyExpand
+  expanded.depthFirstTraversalOrderAux #[]
 
 end Graph
