25a2c4ae7c973f81b4d0e3f3311d7f306841f524
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-11 14:05:01 +0200

    Working benchmarking code

--- Graph.lean
+++ Graph.lean
@@ -50,23 +50,37 @@ def randomNecessaryFunctionForComment := 5
 --   -- IO.println (graph.depthFirstTraversalOrderWithLeaving 0)
 
 
-def bitcoinSize := "../generated-graphs/stanford-bitcoin-sized-topsort-gen.txt"
+def bitcoinSize := "../benchmarking/generated-graphs/stanford-bitcoin-sized-topsort-gen.txt"
 def maximumSize := "../benchmarking/generated-graphs/maximum-working-size-topsort-gen.txt"
-def mediumDense := "../generated-graphs/medium-dense-topsort-gen.txt"
-def mediumVeryDense := "../generated-graphs/medium-very-dense-topsort-gen.txt"
-def mediumSparse := "../generated-graphs/medium-sparse-topsort-gen.txt"
-def mediumVerySparse := "../generated-graphs/medium-very-sparse-topsort-gen.txt"
-def smallDense := "../generated-graphs/small-dense-topsort-gen.txt"
-def smallSparse := "../generated-graphs/small-sparse-topsort-gen.txt"
+def mediumDense := "../benchmarking/generated-graphs/medium-dense-topsort-gen.txt"
+def mediumVeryDense := "../benchmarking/generated-graphs/medium-very-dense-topsort-gen.txt"
+def mediumSparse := "../benchmarking/generated-graphs/medium-sparse-topsort-gen.txt"
+def mediumVerySparse := "../benchmarking/generated-graphs/medium-very-sparse-topsort-gen.txt"
+def smallDense := "../benchmarking/generated-graphs/small-dense-topsort-gen.txt"
+def smallSparse := "../benchmarking/generated-graphs/small-sparse-topsort-gen.txt"
+def huge := "../benchmarking/generated-graphs/huge-topsort-gen.txt"
 
--- def main (argv : List String) : IO Unit := do
 
---   IO.println argv
---   let filePath := maximumSize
+-- Wrt the `timeit` shenanigans: there could be all kinds of things going
+-- on behind the scenes that would make this benchmark fail. E.g. maybe the
+-- compiler is floating a `let` out of the `timeit` action, so it doesn't
+-- measure anything. To guard against that, you could try to turn the `let
+-- x := topsort y` into `let x <- pure (topsort x)`; maybe the compiler is
+-- more reluctant to float out an IO action. Maybe also put a `print`
+-- behind the `topsort` within the `timeit` to force evaluation at this point.
 
---   let graph <- parseGraphFromEdgeList filePath
---   -- IO.println (graph)
---   -- IO.println (graph.depthFirstTraversalOrderWithLeaving 0)
---   timeit "message: " do
---     IO.sleep 100
---     -- IO.println (sorted.get![0])
+
+
+def main (argv : List String) : IO Unit := do
+
+  let filePath := huge
+
+  let graph <- parseGraphFromEdgeList filePath
+  IO.println graph.vertices.back.payload
+  IO.println "Parsed graph"
+  let start <- IO.monoMsNow
+  let res <- graph.topSort
+  let stop <- IO.monoMsNow
+  IO.println ("Sorted graph in: " ++ (toString (stop - start)) ++ " ms")
+  IO.println (res.get![0])
+  
