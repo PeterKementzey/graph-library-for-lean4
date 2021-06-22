b54a57d94e7e132a6806e7197a6360b938bd27ba
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-22 22:32:45 +0200

    Clean up main file

--- Graph.lean
+++ Graph.lean
@@ -13,16 +13,5 @@ import Graph.UndirectedGraph
 -- import Graph.TraverseExample
 -- import Graph.TraverseDeprecated
 
--- def bitcoinSize := "../benchmarking/generated-graphs/stanford-bitcoin-sized-topsort-gen.txt"
--- def maximumSize := "../benchmarking/generated-graphs/maximum-working-size-topsort-gen.txt"
--- def mediumDense := "../benchmarking/generated-graphs/medium-dense-topsort-gen.txt"
--- def mediumVeryDense := "../benchmarking/generated-graphs/medium-very-dense-topsort-gen.txt"
--- def mediumSparse := "../benchmarking/generated-graphs/medium-sparse-topsort-gen.txt"
--- def mediumVerySparse := "../benchmarking/generated-graphs/medium-very-sparse-topsort-gen.txt"
--- def smallDense := "../benchmarking/generated-graphs/small-dense-topsort-gen.txt"
--- def smallSparse := "../benchmarking/generated-graphs/small-sparse-topsort-gen.txt"
--- def huge := "../benchmarking/generated-graphs/huge-topsort-gen.txt"
--- def testGraph := "../benchmarking/generated-graphs/test-topsort-gen.txt"
-
 -- def main (argv : List String) : IO Unit := do
 --   IO.println exampleGraph5.edgeCount
